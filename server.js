5a380f156cd44d924160065acd0b9243bfe222de
ivanchen <2225490226@qq.com>
2020-04-12 22:02:00 +0800

    第二次部署

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ var server = http.createServer(function (request, response) {
                 <link rel="stylesheet" href="/x">
             </head>
             <body>
-                <h1>标题</h1>
+                <h1>简历</h1>
                 <script src="/y"> </script>
             </body>
         `);
